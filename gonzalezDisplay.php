89bab3424df1d9720dce7b8bf3294e6f9cdd1e25
Zigmas Maloni <zmaloni@gmail.com>
2015-03-12 18:31:18 -0500

    adding new 2015 season for all displays

--- gonzalezDisplay.php
+++ gonzalezDisplay.php
@@ -29,13 +29,14 @@
 
   <div id="RPI_Holder">
   <form action="gonzalezDisplay.php" method="post" id="rpi_form">
-      <select name="year" value="options">
-	<option value="Select season...">Select season...</option>
-	<option value="2010">2010/2011 Season</option>
-	<option value="2011">2011/2012 Season</option>
-	<option value="2012">2012/2013 Season</option>
-	<option value="2013">2013/2014 Season</option>
-      </select>
+    <select name="year" value="options">
+		<option value="Select season...">Select season...</option>
+		<option value="2014">2014/2015 Season</option>
+		<option value="2013">2013/2014 Season</option>
+		<option value="2012">2012/2013 Season</option>
+		<option value="2011">2011/2012 Season</option>
+		<option value="2010">2010/2011 Season</option>
+    </select>
       <input type="submit" value="Calculate" id="btn_calc">
     </form>
     <div id="label">Gonzalez RPI ratings for the <?php echo $GONZALEZ->label; ?> season.</div>
