468416a7f839ce312679a88d463a036202d4e222
Vanswa Garbutt <vgarbutt@gmail.com>
2015-10-04 10:17:19 -0500

    Resolving Conflicts
    Resolving Conflicts

---
+++ perroneDisplay.php
@@ -0,0 +1,48 @@
+<?php  
+  require "libs/perrone.php";
+  $PERRONE = new PERRONE();
+  if($_POST){
+    $PERRONE->setStartingSeason($_POST["year"]);
+  } else {
+    $PERRONE->setStartingSeason("Select season...");
+  }
+  $PERRONE->sort();
+?>
+
+<!doctype html>
+<html>
+<head>
+
+  <title>NCDA Perrone Ratings: <?php echo $PERRONE->label; ?> season</title>
+   <LINK href="css/andys_sweet_styles.css" rel="stylesheet" type="text/css">
+   
+</head>
+<body >
+
+  <div id="RPI_Holder">
+  <form action="perroneDisplay.php" method="post" id="rpi_form">
+    <select name="year" value="options">
+		<option value="Select season...">Select season...</option>
+		<option value="2014">2014/2015 Season</option>
+		<option value="2013">2013/2014 Season</option>
+		<option value="2012">2012/2013 Season</option>
+		<option value="2011">2011/2012 Season</option>
+		<option value="2010">2010/2011 Season</option>
+    </select>
+      <input type="submit" value="Calculate" id="btn_calc">
+    </form>
+    <div id="label">Perrone ratings for the <?php echo $PERRONE->label; ?> season.</div>
+	<table>
+      <?php
+	for($i=0;$i<$PERRONE->League->getNumOfTeams();$i++){
+	  $team =  $PERRONE->League->getTeamByIndex($i) ?>
+	  <tr>
+	    <td> <?php echo $i+1 ?></td>
+		<td class="li_name"><?php echo $team->getName()?></td>
+	    <td class="li_rating"><?php echo number_format($team->getRating(), 5)?></td>
+	  </tr>
+      <?php  } ?>  
+    </table>
+  </div>
+</body>
+</html>
---
+++ winlossDisplay.php
@@ -0,0 +1,47 @@
+<?php  
+  require "libs/WinLoss.php";
+  $WinLoss = new WinLoss();  
+  if($_POST){
+    $WinLoss->setStartingSeason($_POST["year"]);
+  } else {
+    $WinLoss->setStartingSeason("Select season...");
+  }
+  $WinLoss->calculate();
+?>
+
+<!doctype html>
+<html>
+<head>
+  <title>NCDA WinLoss Ratings: <?php echo $WinLoss->label; ?> season</title>
+   <LINK href="css/andys_sweet_styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <div id="RPI_Holder">
+  <form actiom="WinLossDisplay.php" method="post" id="WL_form">
+    <select name="year" value="options">
+		<option value="Select season...">Select season...</option>
+		<option value="2014">2014/2015 Season</option>
+		<option value="2013">2013/2014 Season</option>
+		<option value="2012">2012/2013 Season</option>
+		<option value="2011">2011/2012 Season</option>
+		<option value="2010">2010/2011 Season</option>
+    </select>
+      <input type="submit" value="Calculate" id="btn_calc">
+    </form>
+    <div id="label">WinLoss ratings for the <?php echo $WinLoss->label; ?> season.</div>
+	<table>
+      <?php
+	for($i=0;$i<$WinLoss->League->getNumOfTeams();$i++){
+	  $team =  $WinLoss->League->getTeamByIndex($i) ?>
+	  <tr>
+	    <td> <?php echo $i+1 ?></td>
+		<td class="li_name"><?php echo $team->getName()?></td>
+	    <td class="li_rating"><?php echo number_format($team->getRating(), 3) * 100 ?>%</td>
+		<td class="li_test"><?php echo $team->getGamesPlayed()?></td>
+		<td class="li_test"><?php echo $team->getId()?></td>
+	  </tr>
+      <?php  } ?>  
+    </table>
+  </div>
+</body>
+</html>
