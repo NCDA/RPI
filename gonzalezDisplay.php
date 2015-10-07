41e508a013b2188b96a071a23d79561d5bab6788
Vanswa Garbutt <vgarbutt@gmail.com>
2015-10-07 17:50:20 -0500

    Fixing Conflicts
    Fixing Conflicts

--- gonzalezDisplay.php
+++ gonzalezDisplay.php
@@ -1,23 +1,26 @@
 <?php  
-  require "libs/NHL.php";
-  $NHL = new NHL();  
+  require "libs/gonzalez.php";
+  $GONZALEZ = new GONZALEZ();
   if($_POST){
-    $NHL->setStartingSeason($_POST["year"]);
+    $GONZALEZ->setStartingSeason($_POST["year"]);
   } else {
-    $NHL->setStartingSeason("Select season...");
+    $GONZALEZ->setStartingSeason("Select season...");
   }
-  $NHL->calculate();
+  $GONZALEZ->sort();
 ?>
 
 <!doctype html>
 <html>
 <head>
-  <title>NCDA NHL Ratings: <?php echo $NHL->label; ?> season</title>
+
+  <title>NCDA Gonzalez Ratings: <?php echo $GONZALEZ->label; ?> season</title>
    <LINK href="css/andys_sweet_styles.css" rel="stylesheet" type="text/css">
+   
 </head>
-<body>
+<body >
+
   <div id="RPI_Holder">
-  <form actiom="NHLDisplay.php" method="post" id="NHL_form">
+  <form action="gonzalezDisplay.php" method="post" id="rpi_form">
     <select name="year" value="options">
 		<option value="Select season...">Select season...</option>
 		<option value="2015">2015/2016 Season</option>
@@ -29,20 +32,16 @@
     </select>
       <input type="submit" value="Calculate" id="btn_calc">
     </form>
-    <div id="label">NHL ratings for the <?php echo $NHL->label; ?> season.</div>
+    <div id="label">Gonzalez RPI ratings for the <?php echo $GONZALEZ->label; ?> season.</div>
 	<table>
       <?php
-	for($i=0;$i<$NHL->League->getNumOfTeams();$i++){
-	  $team =  $NHL->League->getTeamByIndex($i) ?>
-	 
-	   <tr>
+	for($i=0;$i<$GONZALEZ->League->getNumOfTeams();$i++){
+	  $team =  $GONZALEZ->League->getTeamByIndex($i) ?>
+	  <tr>
 	    <td> <?php echo $i+1 ?></td>
 		<td class="li_name"><?php echo $team->getName()?></td>
-	    <td class="li_rating"><?php echo number_format($team->getRating(), 5) ?></td>
-		<td class="li_test"><?php echo $team->getGamesPlayed()?></td>
-		<td class="li_test"><?php echo $team->getId()?></td>
+	    <td class="li_rating"><?php echo number_format($team->getRating(), 5)?></td>
 	  </tr>
-	  
       <?php  } ?>  
     </table>
   </div>
